f109347ad656fee49ed2077e1c01998e2091e3fb
aarathishan <aarathi.kalai@gmail.com>
2022-09-02 21:28:52 +0530

    Pojo Task

---
+++ res/Types.java
@@ -0,0 +1,15 @@
+package org.pojo.res;
+
+public class Types {
+	
+	private String types;
+
+	public String getTypes() {
+		return types;
+	}
+
+	public void setTypes(String types) {
+		this.types = types;
+	}
+
+}
